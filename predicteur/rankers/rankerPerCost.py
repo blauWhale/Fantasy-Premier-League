20c1358c05394f5cc429e3e4a85b4fa0751c090d
blauWhale <80290651+blauWhale@users.noreply.github.com>
2023-08-10 11:36:44 +0000

    CSVs of rankings per 90 and cost

--- predicteur/rankers/rankerPerCost.py
+++ predicteur/rankers/rankerPerCost.py
@@ -12,12 +12,12 @@ with open('../../data/2022-23/cleaned_players.csv', 'r') as csv_file:
 # Find the player with the most minutes
 most_minutes = max(int(player['minutes']) for player in data)
 
-# Calculate points per 90 minutes for players with enough minutes
+# Calculate points per cost for players with enough minutes
 valid_players = []
 for player in data:
-    if int(player['minutes']) > most_minutes / 2:
-        points_per_90 = (int(player['total_points']) / int(player['minutes'])) * 90
-        player['points_per_90'] = points_per_90
+    if int(player['minutes']) > most_minutes / 2 and int(player['now_cost']) > 0:
+        points_per_cost = int(player['total_points']) / int(player['now_cost'])
+        player['points_per_cost'] = points_per_cost
         valid_players.append(player)
 
 # Group valid players by position
@@ -25,19 +25,19 @@ position_players = defaultdict(list)
 for player in valid_players:
     position_players[player['element_type']].append(player)
 
-# Sort players within each position by points per 90 minutes
+# Sort players within each position by points per cost
 for position, players in position_players.items():
-    position_players[position] = sorted(players, key=lambda x: x['points_per_90'], reverse=True)
+    players.sort(key=lambda x: x['points_per_cost'], reverse=True)
 
 # Create a folder for output files
-output_folder = 'player_rankings_per_90'
+output_folder = 'player_rankings_cost'
 os.makedirs(output_folder, exist_ok=True)
 
 # Write data to separate CSV files for each position
 for position, players in position_players.items():
     output_filename = os.path.join(output_folder, f'player_rankings_{position}.csv')
     with open(output_filename, 'w', newline='') as output_csv:
-        fieldnames = ['Position', 'Rank', 'Name', 'Points per 90 Minutes', 'Now Cost']
+        fieldnames = ['Position', 'Rank', 'Name', 'Points per Cost', 'Now Cost']
         csv_writer = csv.DictWriter(output_csv, fieldnames=fieldnames)
         csv_writer.writeheader()
 
@@ -46,7 +46,7 @@ for position, players in position_players.items():
                 'Position': position,
                 'Rank': rank,
                 'Name': f"{player['first_name']} {player['second_name']}",
-                'Points per 90 Minutes': f"{player['points_per_90']:.2f}",
+                'Points per Cost': f"{player['points_per_cost']:.2f}",
                 'Now Cost': player['now_cost']
             })
 
